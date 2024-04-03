471509ec1a091987d3aa3e2885e328bdfe25d5ec
KeeganDavis <keegan.davis96@gmail.com>
2024-04-03 15:53:34 -0500

    added comments to notebook, added another possible url to js, and minor updates to html and css

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,7 @@
-const url = 'https://keegandavis.github.io/disaster-data-json/emdat_no_drought_or_xtremetemps.json'
+// no drought or xtreme temp data
+// const url = 'https://keegandavis.github.io/disaster-data-json/emdat_no_drought_or_xtremetemps.json'
+
+// all data
+const url = 'https://keegandavis.github.io/disaster-data-json/emdat_cleaned.json'
 
 d3.json(url).then(data => console.log(data))
