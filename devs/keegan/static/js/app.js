0fe54a90dd2f170b8a2ee97c502af367791258bd
KeeganDavis <keegan.davis96@gmail.com>
2024-04-08 19:49:59 -0500

    added connor"s code to app.js and added Bokeh library to html. still need to add his js

--- devs/keegan/static/js/app.js
+++ devs/keegan/static/js/app.js
@@ -36,7 +36,7 @@ function newSelection(vizType, disastersMapData) {
       } else if (vizType == 'map') {
         // if the value of the selection is map, update the html with the correct tags and attributes for the map to be displayed when the function is called
         div.html(
-          ` <div class="p-2" id="visual1-col">
+          ` <div class="p-2">
                 <h2 class="text-center">Map of All Natural Disasters Filtered by Disaster Type</h2><hr>
                 <div id="allDisastersMap" style="border-radius: 15px; border: 3px solid red" margin="5px"></div>
                 <footer>*Disasters only counted when one of the following criteria are met: 10 fatalities,
@@ -44,19 +44,26 @@ function newSelection(vizType, disastersMapData) {
             </div> `
         );
        initLayersAndMarkers(disastersMapData); 
-      } else if (vizType == 'stackedBar') {
+      } else if (vizType == 'timeLine') {
         // if the value of the selection is stackedBar, update the html with the correct tags and attributes for the stacked bar graph to be displayed when the function is called
         div.html(
-          `<div class="col-lg-10">
-                <div id="disastersByContinent" class="visuals"></div>
+          ` <div class="d-flex justify-content-center">
+              <div id="d4f31d05-c7ac-4b36-87c3-c78fa2cb5cb9" data-root-id="p1127" style="display: contents;"></div>
             </div>
-            <div class="col-lg-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>`
-        );
-        stackedBarChart(disastersMapData);
+            <p style="margin-top: -125px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.text</p>`
+        )
+        cl_plot1();
+      } else if (vizType == 'byRegion') {
+        div.html(
+          ` <div class="d-flex justify-content-center">
+              <div id="e17c7ed2-0692-4c9e-b205-c2d57b1ac742" data-root-id="p1274" style="display: contents;"></div>
+            </div>
+            <p style="margin-top: -125px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>`
+        )
+        cl_plot2();
       }
 
     }
